a384f0469f81d8a1493da67a71cf813f952503da
Vivian Stachowiak <vivian.stachowiak00@orange.fr>
2026-02-12 11:28:43 +0100

    IMPORT PROJET SUR ECLIPSE

---
+++ src/test/java/com/ipi/jva350/model/SalarieAideADomicileTest.java
@@ -0,0 +1,14 @@
+package com.ipi.jva350.model;
+
+import jakarta.persistence.*;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+public class SalarieAideADomicileTest {
+
+}
